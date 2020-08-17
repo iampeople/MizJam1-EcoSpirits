44b3bb7551d5335764f5b77ca780ca6453f7fede
iampeople <iampeople@gmail.com>
2020-08-17 00:03:21 -0500

    Add character
    Animate sprite (idle, walk)
    Added movement script (not working)

---
+++ Issues_Found.md
@@ -0,0 +1,14 @@
+#Problems with Godot
+
+##Problem: 
+Textures are fuzzy
+
+##Solution:
+Disable Filter on Import tab of the image and reimport.
+
+
+##Problem
+Computer freezing randomly
+
+##Solution
+Changed from GLES3 to GLES2 (poor intel graphics card on this laptop)
---
+++ Player.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+const move_speed = 40
+var velo = Vector2()
+var drag = 0.5
+
+var facing_right = true
+
+onready var anim_player = $AnimationPlayer
+
+func _physics_process(delta):
+	var move_vec = Vector2()
+	if Input.is_action_pressed("move_left"):
+		move_vec.x -= 1
+	if Input.is_action_pressed("move_right"):
+		move_vec.x += 1
+	if Input.is_action_pressed("move_down"):
+		move_vec.y -= 1
+	if Input.is_action_pressed("move_up"):
+		move_vec.y += 1
+		
+	velo += move_vec * move_speed - drag * velo
+	move_and_slide(velo, Vector2.UP)
+	
+	if move_vec == Vector2():
+		play_anim("idle")
+	else:
+		play_anim("walk")
+	
+func flip():
+	$Sprite.flip_h = !$Sprite.flip_h
+	facing_right = !facing_right
+	
+func play_anim(anim):
+	if anim_player.current_animation == anim:
+		return
+	anim_player.play(anim)
